30c673b824dc77bdb6946daaf2d4078b9f4e2a2b
madevara24 <adityadevara91@gmail.com>
2018-12-24 13:58:32 +0700

    Video 4 : Blade Layout File

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title','About Us')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies libero bibendum, 
+        facilisis sapien id, tincidunt libero. Suspendisse ipsum odio, ullamcorper nec fringilla sed, 
+        placerat non eros. Duis odio libero, consequat vel ultrices nec, varius vel ante. Curabitur quis 
+        eros nulla. Sed consequat massa arcu, eu finibus massa accumsan eu. Maecenas aliquet, leo a 
+        fermentum viverra, turpis dui suscipit magna, id malesuada neque dolor a dolor. Quisque et iaculis 
+        tortor. Quisque semper, nulla eget eleifend luctus, erat arcu aliquam nisl, non faucibus lectus 
+        nunc elementum lectus. Donec porta lacinia felis, et aliquam dui porta in. Nulla luctus laoreet 
+        turpis a dapibus. Quisque fringilla, erat et aliquet sagittis, lectus augue maximus nulla, id 
+        suscipit velit est sit amet nibh. Pellentesque facilisis sed enim sit amet varius. Duis auctor 
+        est in nisl facilisis, ornare fringilla dui aliquam. Fusce diam nibh, accumsan nec nibh at, 
+        vulputate aliquam tellus. Praesent sit amet iaculis odio, quis condimentum nunc.</p>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <title>@yield('title', 'Laracast')</title>
+    </head>
+    <body>
+        
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+
+        @yield('content')
+
+    </body>
+</html>
